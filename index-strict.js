6c3f400f1f34192621339ae371880f02306c6dd3
Dany Christopher PERRIERE <perrierechristopher@gmail.com>
2023-06-26 17:54:14 -0300

    file to lint with "strict"

---
+++ index-strict.js
@@ -0,0 +1,39 @@
+// Primary file for the API
+
+// Dependencies
+const server = require('./lib/server')
+const workers = require('./lib/worker')
+const cli = require("./lib/cli")
+
+// Declare app
+let app = {}
+
+// Declare a global (that strict mode should catch)
+"use strict"
+foo = 'bar'
+
+// Init function
+app.init = () => {
+    // Start server
+    server.init()
+
+    // Start worker
+    workers.init()
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(()=>{
+        cli.init()
+    }, 50)
+}
+
+// Execute
+app.init()
+
+
+
+
+
+// Export the app
+module.exports = app
+
+
