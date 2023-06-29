00c65a6dfdb5d188ed9d0d027c13dad29a79fd12
Dany-Christopher <perrierechristopher@gmail.com>
2023-06-29 02:43:47 +0000

    Cluster file implemented with cluster functions

--- index-cluster.js
+++ index-cluster.js
@@ -1,39 +1,44 @@
 // Primary file for the API
 
 // Dependencies
-const server = require('./lib/server')
-const workers = require('./lib/worker')
-const cli = require("./lib/cli")
+const server = require("./lib/server");
+const workers = require("./lib/worker");
+const cli = require("./lib/cli");
+const cluster = require("cluster");
+const os = require("os");
 
 // Declare app
-let app = {}
+let app = {};
 
 // Init function
 app.init = (callback) => {
-    // Start server
-    server.init()
 
+  // If on the master thread, start background workers and CLI
+  if (cluster.isMaster) {
     // Start worker
-    workers.init()
+    workers.init();
 
     // Start the CLI, but make sure it starts last
-    setTimeout(()=>{
-        cli.init()
-        callback()
-    }, 50)
-}
+    setTimeout(() => {
+      cli.init();
+      callback();
+    }, 50);
+
+    // Fork the process
+
+    for(let i = 0; i < os.cpus().length; i++) {
+      cluster.fork()
+    }
+  } else {
+    // Start server
+    server.init();
+  }
+};
 
 // Slef invoking only if required directly
-if(require.main === module) {
-    app.init(()=>{})
+if (require.main === module) {
+  app.init(() => {});
 }
 
-
-
-
-
-
 // Export the app
-module.exports = app
-
-
+module.exports = app;
